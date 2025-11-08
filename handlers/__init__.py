033035c28c917dbdfb6f05814e52c51305941d24
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-08 09:17:48 +0700

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,8 @@
+# ensure handlers register to bot
+from . import start
+from . import help
+from . import owner
+from . import subowner
+from . import product
+from . import payment
+from . import tagall_admin
