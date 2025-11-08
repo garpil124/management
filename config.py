7e1588eecc83bfab3f0cd7014292b121992d2228
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-09 04:15:18 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID", "0"))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+try:
+    API_ID = int(os.getenv("API_ID"))  
+except:
+    raise SystemExit("❌ API_ID invalid atau belum di set di .env")
 
-MONGO_URI = os.getenv("MONGO_URI", "")
-LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID", "0") or 0)
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+if not API_HASH or not BOT_TOKEN:
+    raise SystemExit("❌ API_HASH atau BOT_TOKEN belum di set di .env")
+
+try:
+    OWNER_ID = int(os.getenv("OWNER_ID"))
+except:
+    raise SystemExit("❌ OWNER_ID invalid atau belum di set di .env")
+
+MONGO_URI = os.getenv("MONGO_URI")
+if not MONGO_URI:
+    raise SystemExit("❌ MONGO_URI belum di set di .env")
+
+try:
+    LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID", "0"))
+except:
+    LOG_CHAT_ID = 0
+
+# Backup & scheduler settings
 BACKUP_FOLDER = os.getenv("BACKUP_FOLDER", "backups")
 BACKUP_HOUR = int(os.getenv("BACKUP_HOUR", "23"))
 BACKUP_MINUTE = int(os.getenv("BACKUP_MINUTE", "55"))
