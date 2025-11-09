3286383248f14f75f6dda3ef8a81321fd45eef91
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-09 15:05:48 +0700

    Update mongo.py

--- db/mongo.py
+++ db/mongo.py
@@ -1,47 +1,34 @@
-import os
 import logging
 from motor.motor_asyncio import AsyncIOMotorClient
+from config import MONGO_URI
 
 logger = logging.getLogger("MongoDB")
-
-MONGO_URI = os.getenv("MONGO_URI")
-
-if not MONGO_URI:
-    logger.error("❌ MONGO_URI belum di set di .env!")
-    raise SystemExit("MONGO_URI not found in environment variables")
 
 client: AsyncIOMotorClient = None
 db = None
 
-# Collections:
 users_col = None
 partners_col = None
 backup_col = None
 payments_col = None
 products_col = None
-orders_col = None  # ✅ WAJIB ditambah biar handler user ga error
+orders_col = None
+subowners_col = None
 
-
-async def init_mongo(uri: str):
+def init_mongo():
     global client, db
-    global users_col, partners_col, backup_col, payments_col, products_col, orders_col
+    global users_col, partners_col, backup_col, payments_col, products_col, orders_col, subowners_col
 
     logger.info("🔗 Connecting to MongoDB...")
-    client = AsyncIOMotorClient(uri)
+    client = AsyncIOMotorClient(MONGO_URI)
     db = client["management_bot"]
 
     users_col = db["users"]
     partners_col = db["partners"]
+    subowners_col = db["subowners"]
     backup_col = db["backups"]
     payments_col = db["payments"]
     products_col = db["products"]
-    orders_col = db["orders"]  # ✅ collection baru buat order user
+    orders_col = db["orders"]
 
-    logger.info("✅ MongoDB connected successfully!")
-
-
-def get_client():
-    return client
-
-def get_db():
-    return db
+    logger.info("✅ MongoDB connected")
