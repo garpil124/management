cf45847520380d61e00c6e9705bc7a585abc3c3d
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-11 01:39:11 +0700

    Update __init__.py

--- callback/__init__.py
+++ callback/__init__.py
@@ -1,5 +1,10 @@
-from .owner_cb import *
-from .subowner_cb import *
-from .user_cb import *
-from .product_cb import *
-from .payment_cb import *
+from .owner_cb import register_owner_callback
+from .payment_cb import register_payment_callback
+from .subowner_cb import register_subowner_callback
+from .user_cb import register_user_callback
+
+def register_callback_handlers(app):
+    register_owner_callback(app)
+    register_payment_callback(app)
+    register_subowner_callback(app)
+    register_user_callback(app)
