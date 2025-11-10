3325656912cbfe62bfcb1bc4bacd78d1cb44b206
garpil124 <kurniawansyahrifky122@gmail.com>
2025-11-10 15:59:42 +0700

    Update subowner.py

--- handlers/subowner.py
+++ handlers/subowner.py
@@ -1,4 +1,4 @@
-        from pyrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import (
     Message, CallbackQuery,
     InlineKeyboardMarkup, InlineKeyboardButton,
